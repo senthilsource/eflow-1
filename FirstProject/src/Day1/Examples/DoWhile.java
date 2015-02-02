c35d1e2df42d3969837e2b78d5f99456740b612c
sharadrao <sharadrao@ba18f5f1-b81a-47f2-80d7-d788c241dd9b>
2015-02-02 14:48:47 +0000

    sharad day-1 addition

---
+++ FirstProject/src/Day1/Examples/DoWhile.java
@@ -0,0 +1,19 @@
+package Day1.Examples;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int x = 10;
+		do{
+			System.out.println( "x = " + x);
+			x--;
+			
+		}while(x >0);
+		//int x=1;
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
