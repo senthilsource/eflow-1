5c71bedce6e13bda00810f99ce67be92864b4e9c
sharadrao <sharadrao@ba18f5f1-b81a-47f2-80d7-d788c241dd9b>
2015-02-02 17:07:48 +0000

    Feb2 first program

---
+++ FirstProject/src/Day1/Examples/feb2.java
@@ -0,0 +1,19 @@
+package Day1.Examples;
+
+public class feb2 {
+	// Example of a while loop
+	
+	public static void main(String[] args) {
+		int x =1;
+		System.out.println("Before the loop");
+		while (x <4){
+		System.out.println("In the loop");
+		System.out.println("Value of x is " + x);
+		 x = x+1;
+		}
+		System.out.println("This is after the loop");
+		// TODO Auto-generated method stub
+
+	}
+
+}
